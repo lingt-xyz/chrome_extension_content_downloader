1ecdb3bcb3ba560fe961ad6a03e0211cd1e0288c
Ling Tan <lingt.xyz@gmail.com>
2021-09-14 20:50:59 -0400

    trying to add PDF support

--- popup.js
+++ popup.js
@@ -16,18 +16,42 @@ function addImages(items) {
   page.appendChild(imageDiv);
 }
 
-function downloadCheckedLinks() {
+function downloadAllImages() {
   for (let item of links) {
     chrome.downloads.download({url: item}, function(id) {});
   }
   window.close();
 }
 
+function downloadAsPDF() {
+  $('body').modal('show');
+  setTimeout(function () {
+      html2canvas(document.querySelector("body")).then(canvas => {
+          //$("#previewBeforeDownload").html(canvas);
+          var imgData = canvas.toDataURL("image/jpeg",1);
+          var imageWidth = canvas.width;
+          var imageHeight = canvas.height;
+
+          var pdf = new jsPDF("p", "mm", "a4");
+          var pageWidth = pdf.internal.pageSize.getWidth();
+          var pageHeight = pdf.internal.pageSize.getHeight();
+
+          var ratio = imageWidth/imageHeight >= pageWidth/pageHeight ? pageWidth/imageWidth : pageHeight/imageHeight;
+          //pdf = new jsPDF(this.state.orientation, undefined, format);
+          pdf.addImage(imgData, 'JPEG', 0, 0, imageWidth * ratio, imageHeight * ratio);
+          pdf.save("invoice.pdf");
+          //$("#previewBeforeDownload").hide();
+          $('body').modal('hide');
+      });
+  },500);
+}
+
 window.onload = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {options: presetOptions}, function(response) {
       addImages(response.items);
-      document.getElementById('download0').onclick = downloadCheckedLinks;
+      document.getElementById('download_img').onclick = downloadAllImages;
+      document.getElementById('download_pdf').onclick = downloadAsPDF;
     });
   });
 };
